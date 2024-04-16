5f892ff65baa3fe6be9093609eaf9cc2ce4297a6
Kacper <kacperjanzieba@gmail.com>
2024-04-16 21:47:30 +0200

    add tests and hotjar script

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,33 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if user can go to previous slide', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.wait(2000);
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if all slides contents are corrrect', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'France');
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
